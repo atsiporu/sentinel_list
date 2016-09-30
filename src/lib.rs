557b399ab756e920e965cde0b8a0d1bee8d30ae4
Andrey Tsiporukha <Andrey@Tsiporukha.com>
2016-09-29 21:28:52 -0400

    Adding public interface

--- src/lib.rs
+++ src/lib.rs
@@ -6,145 +6,235 @@
 //!
 //! ```rust
 //! extern crate sentinel_list;
-//! use sentinel_list::{new_sentinel, new_handle, insert_after, debug_print};
+//! use sentinel_list::*;
 //!
 //! fn main() {
-//!     let mut s = new_sentinel();
-//!     let mut h1 = new_handle(1);
-//!     let mut h2 = new_handle(2);
-//!     let mut h3 = new_handle(3);
-//!     let mut h4 = new_handle(4);
-//! 
-//!     println!("\nObjects:");
-//!     println!("{:p} {:?}", s, s.value);
-//!     println!("{:p} {:?}", h1, h1.value);
-//!     println!("{:p} {:?}", h2, h2.value);
-//!     println!("{:p} {:?}", h3, h3.value);
-//!     println!("{:p} {:?}", h4, h4.value);
-//!     
-//!     insert_after(&mut s, &mut h1);
-//!     insert_after(&mut h1, &mut h2);
-//!     insert_after(&mut h2, &mut h3);
-//!     insert_after(&mut h3, &mut h4);
-//!     
-//!     println!("\nLinked:");
-//!     println!("{:p} {:?}", s, s.value);
-//!     println!("{:p} {:?}", h1, h1.value);
-//!     println!("{:p} {:?}", h2, h2.value);
-//!     println!("{:p} {:?}", h3, h3.value);
-//!     println!("{:p} {:?}", h4, h4.value);
-//!     
-//!     
-//!     println!("\nDebug-print:");
-//!     debug_print(&mut s);
+//!	 let l = &mut List::new();
+//!	 let h1 = l.push_head(1);
+//!	 let h2 = l.push_tail(2);
+//!	 let h3 = l.push_tail(3);
+//!	 {
+//!		assert_eq!(&1, (&h1).as_ref());
+//!		assert_eq!(&1, l.peek_head().unwrap());
+//!		assert_eq!(&3, l.peek_tail().unwrap());
+//!	 }
+//!	 h1.unlink();
+//!	 assert_eq!(&2, l.peek_head().unwrap());
+//!	 h3.unlink();
+//!	 assert_eq!(&2, l.peek_tail().unwrap());
+//!	 h2.unlink();
+//!	 assert_eq!(None, l.peek_tail());
+//!	 assert_eq!(None, l.peek_head());
 //! }
 //! ```
 
-pub use self::sentinel_list::{new_handle, new_sentinel, Handle, insert_after, debug_print};
+#![feature(conservative_impl_trait)]
 
-mod sentinel_list
+use std::ptr;
+use std::fmt;
+use std::ops::Deref;
+
+
+pub trait ListHandle<T>
 {
-	use std::ptr;
-	use std::fmt::Debug;
-	
-	pub struct Link<T>
-	{
-		pub next: *mut Link<T>,
-		pub prev: *mut Link<T>,
-		pub value: Option<T>,
-	}
+	fn unlink(self);
+	fn as_ref(&self) -> &T;
+}
 
-	impl<T> Link<T>
+pub struct List<T>
+{
+	sentinel: Handle<T>,
+}
+
+impl<T> List<T>
+{
+	pub fn new() -> Self
 	{
-		fn new(v: T) -> Link<T>
-		{
-			Link {
-				prev: ptr::null_mut(),
-				next: ptr::null_mut(),
-				value: Some(v),
-			}
+		List {
+			sentinel: new_sentinel(),
 		}
 	}
 
-	pub type Handle<T> = Box<Link<T>>;
-
-	pub fn new_sentinel<T>() -> Handle<T>
+	pub fn push_head(&mut self, e: T) -> impl ListHandle<T>
 	{
-		let mut h = Box::new(
-			Link { 
-				prev: ptr::null_mut(), 
-				next: ptr::null_mut(),
-				value: None,
-			}
-		);
-		h.prev = &mut *h;
-		h.next = &mut *h;
+		let mut h = new_handle(e);
+		insert_after(&mut self.sentinel, &mut h);
 		h
 	}
 
-	pub fn new_handle<T>(v: T) -> Handle<T>
+	pub fn push_tail(&mut self, e: T) -> impl ListHandle<T>
 	{
-		Box::new(Link::new(v))
+		let mut h = new_handle(e);
+		insert_after(unsafe {&mut *self.sentinel.prev}, &mut h);
+		h
 	}
 
-	pub fn insert_after<T>(after: &mut Handle<T>, h: &mut Handle<T>)
+	pub fn peek_head(&self) -> Option<&T>
 	{
-		let n: *mut _ = after.next;
-
-		h.prev = &mut **after;
-		h.next = n;
-		
-		after.next = &mut **h;
-		
-		let n = unsafe { &mut *h.next };
-		n.prev = &mut **h;
+		let link = unsafe { &*self.sentinel.next };
+		link.value.as_ref()
 	}
-
-	pub fn debug_print<T: Debug>(s: &mut Handle<T>)
+	
+	pub fn peek_tail(&self) -> Option<&T>
 	{
-		let mut h = s.next;
-		while h != &mut **s {
-			let n = unsafe { &*h };
-			println!("{:?}", n.value);
-			h = n.next;
+		let link = unsafe { &*self.sentinel.prev };
+		link.value.as_ref()
+	}
+}
+
+#[derive(PartialEq)]
+struct Link<T>
+{
+	pub next: *mut Link<T>,
+	pub prev: *mut Link<T>,
+	pub value: Option<T>,
+}
+
+type Handle<T> = Box<Link<T>>;
+
+impl<T> Link<T>
+{
+	fn new(v: T) -> Link<T>
+	{
+		Link {
+			prev: ptr::null_mut(),
+			next: ptr::null_mut(),
+			value: Some(v),
 		}
 	}
 }
 
-#[cfg(test)]
-mod tests {
-	use sentinel_list::{new_sentinel, new_handle, debug_print, insert_after};
-
-	#[test]
-	fn it_works() {
-        let mut s = new_sentinel();
-        let mut h1 = new_handle(1);
-        let mut h2 = new_handle(2);
-        let mut h3 = new_handle(3);
-        let mut h4 = new_handle(4);
-
-
-        println!("\nObjects:");
-        println!("{:p} {:?}", s, s.value);
-        println!("{:p} {:?}", h1, h1.value);
-        println!("{:p} {:?}", h2, h2.value);
-        println!("{:p} {:?}", h3, h3.value);
-        println!("{:p} {:?}", h4, h4.value);
-
-        insert_after(&mut s, &mut h1);
-        insert_after(&mut h1, &mut h2);
-        insert_after(&mut h2, &mut h3);
-        insert_after(&mut h3, &mut h4);
-        
-        println!("\nLinked:");
-        println!("{:p} {:?}", s, s.value);
-        println!("{:p} {:?}", h1, h1.value);
-        println!("{:p} {:?}", h2, h2.value);
-        println!("{:p} {:?}", h3, h3.value);
-        println!("{:p} {:?}", h4, h4.value);
-
-
-        println!("\nDebug-print:");
-        debug_print(&mut s);
+impl<T: fmt::Debug> fmt::Debug for Link<T>
+{
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "Link {{ prev: {:p}, next: {:p}, value: {:?} }}", self.prev, self.next, self.value)
 	}
+}
+
+impl<T> ListHandle<T> for Handle<T>
+{
+	fn unlink(self)
+	{
+		unlink(self);
+	}
+
+	fn as_ref(&self) -> &T
+	{
+		&self
+	}
+}
+
+impl<T> Deref for Link<T>
+{
+	type Target = T;
+	fn deref(&self) -> &Self::Target
+	{
+		// if you call it on the sentinel 
+		// you deserve to get what's coming
+		self.value.as_ref().unwrap()
+	}
+}
+
+fn new_sentinel<T>() -> Handle<T>
+{
+	let mut h = Box::new(
+		Link { 
+			prev: ptr::null_mut(), 
+			next: ptr::null_mut(),
+			value: None,
+		}
+	);
+	h.prev = &mut *h;
+	h.next = &mut *h;
+	h
+}
+
+fn new_handle<T>(v: T) -> Handle<T>
+{
+	Box::new(Link::new(v))
+}
+
+fn insert_after<T>(after: &mut Link<T>, h: &mut Link<T>)
+{
+	let n: *mut _ = after.next;
+
+	h.prev = &mut *after;
+	h.next = n;
+	
+	after.next = &mut *h;
+	
+	let n = unsafe { &mut *h.next };
+	n.prev = &mut *h;
+}
+
+fn unlink<T>(h: Handle<T>)
+{
+	let prev = unsafe { &mut *h.prev };
+	let next = unsafe { &mut *h.next };
+	next.prev = prev;
+	prev.next = next;
+}
+
+#[cfg(test)]
+fn debug_print<T: fmt::Debug>(s: &mut Handle<T>)
+{
+	let mut h = s.next;
+	while h != &mut **s {
+		let n = unsafe { &*h };
+		println!("{:?}", n);
+		h = n.next;
+	}
+}
+
+#[cfg(test)]
+#[test]
+fn link_unlink()
+{
+	let mut s = new_sentinel();
+	let mut h1 = new_handle(1);
+	let mut h2 = new_handle(2);
+	let mut h3 = new_handle(3);
+	
+	insert_after(&mut s,  &mut h1);
+	insert_after(&mut h1, &mut h2);
+	insert_after(&mut h2, &mut h3);
+
+	assert_eq!(unsafe {&*s.next}, &*h1);
+	assert_eq!(unsafe {&*h1.prev}, &*s);
+
+	assert_eq!(unsafe {&*h1.next}, &*h2);
+	assert_eq!(unsafe {&*h2.prev}, &*h1);
+	
+	assert_eq!(unsafe {&*h2.next}, &*h3);
+	assert_eq!(unsafe {&*h3.prev}, &*h2);
+	
+	assert_eq!(unsafe {&*h3.next}, &*s);
+	assert_eq!(unsafe {&*s.prev}, &*h3);
+
+	unlink(h2);
+
+	assert_eq!(unsafe {&*s.next}, &*h1);
+	assert_eq!(unsafe {&*h1.prev}, &*s);
+	
+	assert_eq!(unsafe {&*h1.next}, &*h3);
+	assert_eq!(unsafe {&*h3.prev}, &*h1);
+	
+	assert_eq!(unsafe {&*h3.next}, &*s);
+	assert_eq!(unsafe {&*s.prev}, &*h3);
+	
+	let mut h2 = new_handle(2);
+	insert_after(&mut h1, &mut h2);
+	
+	unlink(h1);
+	assert_eq!(unsafe {&*s.next}, &*h2);
+	unlink(h2);
+	assert_eq!(unsafe {&*s.next}, &*h3);
+	assert_eq!(unsafe {&*s.prev}, &*h3);
+	assert_eq!(unsafe {&*h3.prev}, &*s);
+	assert_eq!(unsafe {&*h3.next}, &*s);
+	unlink(h3);
+	assert_eq!(unsafe {&*s.prev}, &*s);
+	assert_eq!(unsafe {&*s.next}, &*s);
+	
+	unlink(s);
 }
